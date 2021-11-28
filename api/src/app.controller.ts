5d6d41c07631962006e8ff9edeb40e2a18ffdcfd
gioperalto <gyoperalto@gmail.com>
2021-11-28 12:07:53 -0500

    Updated the wrong package.json (root level instead of api/)

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,15 +1,11 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
   }
-
 }
